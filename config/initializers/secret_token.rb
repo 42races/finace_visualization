8b5dafb6cda71321a478b9249108fe2ca7f8a064
Deepak Kumar <deepakkumarnd@gmail.com>
2013-11-01 10:22:48 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fiscal::Application.config.secret_key_base = 'ce8525a5652da81f01308cd4b5a0b80d289f763cb10dee24d5ece470b7db6810f6a7906ff899d7c92f25a19315089dc5e96ab0647f76e9a09db927ab062b2ff0'
