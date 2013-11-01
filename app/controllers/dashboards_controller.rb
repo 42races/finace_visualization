c3857478e8bfd89116c2ad2e829e03a5e196436c
Deepak Kumar <deepakkumarnd@gmail.com>
2013-11-01 12:19:01 +0530

    temp: test data to plot the graph

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def index
+    parser = SheetParser.new()
+    @data = parser.parse
     render :index
   end
 end
--- app/services/sheet_parser.rb
+++ app/services/sheet_parser.rb
@@ -1,5 +1,32 @@
 class SheetParser
+
+  attr_accessor :record
+
   def initialize
-    @record = Roo::Google.new($KEY, user: $USER, password: $PASSWORD)
+    # @record = Roo::Google.new($KEY, user: $USER, password: $PASSWORD)
+  end
+
+  def parse
+    data = {
+      "10/22/2013"=>199.0,
+      "10/23/2013"=>128.0,
+      "10/24/2013"=>133.0,
+      "10/25/2013"=>80.0,
+      "10/26/2013"=>30.0,
+      "10/27/2013"=>197.0
+    }
+
+    data
+
+    # first = 2
+    # last  = @record.last_row
+
+    # data_hash = {}
+
+    # (first...last).each do |i|
+    #   data_hash[@record.cell('A', i)] = @record.cell('B', i)
+    # end
+
+    # data_hash
   end
 end
