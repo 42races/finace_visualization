485ee70ae474329fd7a1ae567df1bfbeca824456
Deepak Kumar <deepakkumarnd@gmail.com>
2013-11-02 18:52:21 +0530

    update pradeeps email

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -2,7 +2,7 @@ $KEY = '0AsMq-WXCyA_mdFNIdHF2LUxUb0tJNUlFLVpuLWFvV2c'
 
 $emails = [
   'deepakkumarnd@gmail.com',
-  'pradeep@crowdstudio.com',
+  'pradeep@crowdstudio.in',
   'sumesh@wowmakers.com',
   'shibu.pg@wowmakers.com',
   'jithesh@wowmakers.com'
